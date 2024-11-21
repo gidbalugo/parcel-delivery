9d5d9586df77b948ea85dfb9c47469baa04c4f71
Gid Balugo <gid.balugo@gmail.com>
2024-11-21 23:40:08 +0800

    Created DTO to properly display cost of parcel and indicate if it's discounted or not

--- src/main/java/com/example/delivery/dto/ParcelDeliveryCostDto.java
+++ src/main/java/com/example/delivery/dto/ParcelDeliveryCostDto.java
@@ -1,2 +1,15 @@
-package com.example.delivery.dto;public class ParcelDeliveryCostDto {
+package com.example.delivery.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ParcelDeliveryCostDto {
+
+    private double deliveryCost;
+    private boolean voucherApplied;
+
+    public ParcelDeliveryCostDto(Double deliveryCost, boolean isDiscounted) {
+        this.deliveryCost = deliveryCost;
+        this.voucherApplied = isDiscounted;
+    }
 }
