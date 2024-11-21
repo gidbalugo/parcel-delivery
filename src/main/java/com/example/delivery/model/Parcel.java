fbd0f13be82c612a44af1b86e6564d9230ee79ac
Gid Balugo <gid.balugo@gmail.com>
2024-11-21 23:42:48 +0800

    Updated parcel model to include voucher, and fixed discounting of parcel cost

--- src/main/java/com/example/delivery/model/Parcel.java
+++ src/main/java/com/example/delivery/model/Parcel.java
@@ -9,14 +9,16 @@ public class Parcel {
     private final Double height;
     private final Double width;
     private final Double length;
+
+    private final String voucher;
     private final Double volume;
 
-    public Parcel(Double weight, Double height, Double width, Double length) {
+    public Parcel(Double weight, Double height, Double width, Double length, String voucher) {
         this.weight = weight;
         this.height = height;
         this.width = width;
         this.length = length;
+        this.voucher = voucher;
         this.volume = height * width * length;
     }
-
 }
--- src/main/java/com/example/delivery/service/ParcelDeliveryService.java
+++ src/main/java/com/example/delivery/service/ParcelDeliveryService.java
@@ -17,15 +17,24 @@ public class ParcelDeliveryService {
     private final SmallParcelService smallParcelService;
     private final MediumParcelService mediumParcelService;
 
+    private final VoucherService voucherService;
+
     public Double calculateDeliveryCost(ParcelSpecificationDto dto) throws ParcelExceedsWeightLimitException {
         Parcel parcel = new Parcel(dto.getWeight(),
                 dto.getHeight(),
                 dto.getWidth(),
-                dto.getLength());
-        return calculateDeliveryCost(parcel);
+                dto.getLength(),
+                dto.getVoucher());
+
+        Double deliveryCost = determineCostByType(parcel);
+        if (parcel.getVoucher() == null ||parcel.getVoucher().isBlank()) {
+            return deliveryCost;
+        }
+
+        return applyDiscount(deliveryCost, dto.getVoucher());
     }
 
-    public Double calculateDeliveryCost(Parcel parcel) throws ParcelExceedsWeightLimitException {
+    private Double determineCostByType(Parcel parcel) throws ParcelExceedsWeightLimitException {
         if (parcel.getWeight() > parcelConfig.getMaxWeightLimit()) throw new ParcelExceedsWeightLimitException("Parcel is over max weight limit.");
         if (parcel.getWeight() > parcelConfig.getWeightLimitForHeavyParcel()) return heavyParcelService.calculateCost(parcel);
 
@@ -34,5 +43,13 @@ public class ParcelDeliveryService {
         if (volume < parcelConfig.getVolumeLimitForMediumParcel()) return mediumParcelService.calculateCost(parcel);
 
         return largeParcelService.calculateCost(parcel);
+    }
+
+    private Double applyDiscount(Double originalCost, String voucherCode) {
+        Voucher voucher = voucherService.fetchVoucher(voucherCode);
+
+        if (voucher.getDiscount() >= originalCost) return Double.valueOf(0);
+
+        return originalCost - voucher.getDiscount();
     }
 }
