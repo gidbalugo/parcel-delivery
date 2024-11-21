b75e8d88b7d27b9dcf22ff3f00f23a556de74d7e
Gid Balugo <gid.balugo@gmail.com>
2024-11-21 23:43:51 +0800

    Added mock voucher endpoint, and alinged properties file for the externally sourced values

--- src/main/java/com/example/delivery/controller/ParcelDeliveryController.java
+++ src/main/java/com/example/delivery/controller/ParcelDeliveryController.java
@@ -1,15 +1,14 @@
 package com.example.delivery.controller;
 
+import com.example.delivery.dto.ParcelDeliveryCostDto;
 import com.example.delivery.dto.ParcelSpecificationDto;
 import com.example.delivery.exception.ParcelExceedsWeightLimitException;
+import com.example.delivery.model.Voucher;
 import com.example.delivery.service.ParcelDeliveryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +18,22 @@ public class ParcelDeliveryController {
     private final ParcelDeliveryService parcelDeliveryService;
 
     @PostMapping("/calculate")
-    public ResponseEntity<Double > calculate(@Valid @RequestBody ParcelSpecificationDto parcelSpecificationDto) throws ParcelExceedsWeightLimitException {
+    public ResponseEntity<ParcelDeliveryCostDto> calculate(@Valid @RequestBody ParcelSpecificationDto parcelSpecificationDto) throws ParcelExceedsWeightLimitException {
         Double deliveryCost = parcelDeliveryService.calculateDeliveryCost(parcelSpecificationDto);
 
-        return ResponseEntity.ok(deliveryCost);
+        return ResponseEntity.ok(convert(deliveryCost, parcelSpecificationDto));
+    }
+
+    @GetMapping("/voucher/{voucherCode}")
+    public ResponseEntity<Voucher> mockVoucher(@RequestParam String key, @PathVariable("voucherCode") String voucherCode) {
+        Voucher voucher = new Voucher(voucherCode, 100.00, "2024-11-21");
+        return ResponseEntity.ok(voucher);
+    }
+
+    private ParcelDeliveryCostDto convert(Double deliveryCost, ParcelSpecificationDto parcelSpecificationDto) {
+        String voucher = parcelSpecificationDto.getVoucher();
+        boolean isDiscounted = (voucher == null || voucher.isBlank()) ? false: true;
+        return new ParcelDeliveryCostDto(deliveryCost, isDiscounted);
     }
 
 }
