28450bbbb977773932e3d233a46d0d0db62f3a6f
Gid Balugo <gid.balugo@gmail.com>
2024-11-21 23:40:37 +0800

    Updated to include optional 'voucher'

--- src/main/java/com/example/delivery/dto/ParcelSpecificationDto.java
+++ src/main/java/com/example/delivery/dto/ParcelSpecificationDto.java
@@ -1,17 +1,27 @@
 package com.example.delivery.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ParcelSpecificationDto {
 
+    @NotNull
     @Min(value = 1, message = "Weight should be greater than 0")
     private Double weight;
+
+    @NotNull
     @Min(value = 1, message = "Weight should be greater than 0")
     private Double height;
+
+    @NotNull
     @Min(value = 1, message = "Weight should be greater than 0")
     private Double width;
+
+    @NotNull
     @Min(value = 1, message = "Weight should be greater than 0")
     private Double length;
+
+    private String voucher;
 }
